db33c6c6219c4d79046ba9612cdf44cef20c9a0a
Giovana <giovanaaraujo3003@gmail.com>
2021-06-15 16:57:30 -0300

    Inicializando projeto

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.listen(3000, () => console.log("Testando"));
+
+app.get('/atendimentos', (request, response)=>response.send("Você está na rota de atendimentos!"));
