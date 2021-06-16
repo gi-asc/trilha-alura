ce74b79ebeea831008571765b39a5146394d73a7
Giovana Carvalho <giovanaaraujo3003@gmail.com>
2021-06-16 17:08:53 -0300

    Recebendo dados via POST

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,4 +1,8 @@
 module.exports = (app) => {
     app.get('/atendimentos', (request, response)=>response.send("testando GET"));
-    app.post('/atendimentos', (request, response)=>response.send("testando POST"));
+    app.post('/atendimentos', (request, response)=>{
+        console.log(request.body);
+        response.send("testando POST")
+    }
+        );
 }
